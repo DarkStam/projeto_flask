45bb380002ac7f7a58a1d6d1c9a8125e5175be93
stanleydias <stanleycdias@gmail.com>
2025-10-17 20:08:08 -0300

    atualização

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class Task(db.Model):
     title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.String(200), nullable=False)
     done = db.Column(db.Boolean, default=False, nullable=False)
-
+    
+    
     def __repr__(self):
         return f'<Taks {self.title}>'
